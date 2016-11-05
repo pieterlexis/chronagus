5bcfcaa2c51e4e63e06f70326b93e5d9cfb7287a
Rik van Duijn <ik@rikvanduijn.nl>
2016-11-05 15:04:12 +0100

    implements bootsy WYSIWYG on locations

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -45,7 +45,7 @@ class LocationsController < ApplicationController
   end
 
   def location_params
-    input_params = params.require(:location).permit(:name, :public_description, :campaign_id)
+    input_params = params.require(:location).permit(:name, :public_description, :campaign_id, :bootsy_image_gallery_id)
     sanitize_input(input_params, [:name, :public_description])
     input_params
   end
