423c1dd4eb54bf6d66966cf354ef364c1a9ea7d9
Rik van Duijn <ik@rikvanduijn.nl>
2016-11-06 16:29:37 +0100

    Symlinks upload folders to keep them after deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -33,7 +33,7 @@ set :rbenv_roles, :all # default value
 set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
 
 # Default value for linked_dirs is []
-set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads/bootsy', 'public/uploads/store')
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
