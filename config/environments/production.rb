8ff3aeb8235d482d4cf6db5f8a65d3c0338e2687
Rik van Duijn <ik@rikvanduijn.nl>
2017-04-15 20:36:20 +0200

    Set up sendmail config for devise mails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Set sendmail options
+  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.default_url_options = { host: '149.202.106.8'  }
 end
