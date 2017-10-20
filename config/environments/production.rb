f46eac356971ac0d2722e5f625443d26c4a5a7f2
Rik van Duijn <ik@rikvanduijn.nl>
2017-10-21 01:13:55 +0200

    Restore email host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,6 +79,7 @@ Rails.application.configure do
 
   # Set sendmail options
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: 'chronagus.plexis.eu' }
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
